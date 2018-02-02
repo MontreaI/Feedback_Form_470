87d96a19ce91a27b0b99f6597ca94e069b3a1c06
apollo012245 <alexander_yoo@hotmail.com>
2018-02-01 20:25:24 -0800

    Rewrote PHP script to get variables from form

--- forms.php
+++ forms.php
@@ -1,5 +1,20 @@
+<!DOCTYPE HTL>
+<html>
+<head>
+<title>Forms</title>
+</head>
+<body>
+<p>Your ID is: 
 <?php
-$ID = $_POST("ID");
-$FB = $_POST("FB");
-
+$ID = $_POST["ID"];
+echo $ID;
 ?>
+<br></p>
+<p> You wrote: 
+<?php
+$FB = $_POST["FB"];
+echo $FB;
+?>
+<br></p>
+</body>
+</html>
